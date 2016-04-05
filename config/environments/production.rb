adf8249c70e71590bd2103c38413eb3c813d2820
Omar Torres <torresomar44@gmail.com>
2016-04-04 22:06:43 -0500

    minor change in production.rb for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
