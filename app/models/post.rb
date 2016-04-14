7794f73603f0cfae16a0f3a39bc3fc0cb167d84b
Omar Torres <torresomar44@gmail.com>
2016-04-13 23:12:27 -0500

    Modify posts#new action to create a new record in DB

--- app/models/post.rb
+++ app/models/post.rb
@@ -29,6 +29,12 @@ class Post < ActiveRecord::Base
 
   include SearchablePost
 
+  def self.new_draft_for(user)
+    post = self.new(user_id: user.id)
+    post.save_as_draft
+    post
+  end
+
   def self.tagged_with(name)
     Tag.find_by!(name: name).posts
   end
@@ -50,7 +56,7 @@ class Post < ActiveRecord::Base
 
   def save_as_draft
     self.published_at = nil
-    save(validation: false)
+    save(validate: false)
   end
 
   def unpublish
