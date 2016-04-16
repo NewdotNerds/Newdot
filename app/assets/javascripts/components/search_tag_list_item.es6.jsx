0a53ec4d0fdea7144ce508c75f7568bb8c12454e
Omar Torres <torresomar44@gmail.com>
2016-04-16 14:53:45 -0500

    Add Tags to autocompelte dropdown

---
+++ app/assets/javascripts/components/search_tag_list_item.es6.jsx
@@ -0,0 +1,11 @@
+class SearchTagListItem extends React.Component {
+  render () {
+    return (
+      <li>
+        <a href={this.props.tag.url}>
+          <span className="icon-tag"></span> {this.props.tag.name}
+        </a>
+      </li>
+    );
+  }
+}
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,20 @@
 class Tag < ActiveRecord::Base
   has_many :taggings, dependent: :destroy
   has_many :posts, through: :taggings
+  
+  has_many :interests, dependent: :destroy
+  has_many :followers, through: :interests, source: :follower
+
+  include SearchableTag
 end
+
+# Delete the previous posts index in Elasticsearch
+Tag.__elasticsearch__.client.indices.delete index: Tag.index_name rescue nil
+
+# Create the new index with the new mapping
+Tag.__elasticsearch__.client.indices.create \
+  index: Tag.index_name,
+  body: { settings: Tag.settings.to_hash, mappings: Tag.mappings.to_hash }
+
+# Index all post records from the DB to Elasticsearch
+Tag.import
