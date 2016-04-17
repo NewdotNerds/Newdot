5de8e47085de70ed83143848d2c1c09b68b7a758
Omar Torres <torresomar44@gmail.com>
2016-04-17 11:52:18 -0500

    Add elasticsearch create index task

--- app/controllers/api/following_controller.rb
+++ app/controllers/api/following_controller.rb
@@ -1,6 +1,6 @@
 class API::FollowingController < ApplicationController
   def index
-    user = User.find(params[:user_id])
+    user = User.friendly.find(params[:user_id])
     @following = user.friendly.following.paginate(page: params[:page], per_page: 6)
     @current_page = @following.current_page
     @next_page = @following.next_page
