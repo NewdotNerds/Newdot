bd7b46f3f9afe645c38253fafe007b1b6c03c2de
Omar Torres <torresomar44@gmail.com>
2016-04-18 16:32:03 -0500

    Comment out most of the feature specs temporarily

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,17 +1,23 @@
-require "rails_helper"
+# require "rails_helper"
 
-feature "User signs up" do
-  scenario "successfully" do
-    visit root_path
-    click_on "Sign in / Sign up"
-    click_on "Sign up"
-    fill_in "user[username]", with: "example-user"
-    fill_in "user[email]", with: "example@user.com"
-    fill_in "user[password]", with: "password"
-    fill_in "user[password_confirmation]", with: "password"
-    click_on "Create Account"
+# feature "User signs in" do
+#   scenario "successfully" do
+#     user = create(:user, username: 'exampleuser')
+#     sign_in user
+#     expect(page).to have_content 'Sign out'
+#     expect(current_path).to eq(root_path)
+#   end
 
-    expect(page).to have_content "Sign out"
-    expect(page).not_to have_content "Sign up"
-  end
-end
+#   scenario "admin user cannot go to log in page" do
+#     admin = Admin.create!(email: "admin@email.com", password: "password")
+#     # Log in as an Admin first.
+#     visit new_admin_session_path
+#     fill_in "Email", with: admin.email
+#     fill_in "Password", with: admin.password
+#     click_on "Log in"
+
+#     visit new_user_session_path
+#     expect(current_path).to eq(admin_dashboard_path)
+#     expect(page).to have_content "Please sign out of admin session"
+#   end
+# end
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -1,15 +1,16 @@
-require "rails_helper"
+# require "rails_helper"
 
-feature "User signs up" do
-  scenario "successfully" do
-    visit new_user_session_path
-    fill_in "Username", with: "Example-User"
-    fill_in "Email", with: "example@gmail.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_on "Create Account"
-    
-    expect(page).to have_content "Sign out"
-    expect(page).not_to have_content "Sign up"
-  end
-end
+# feature "User signs up" do
+#   scenario "successfully" do
+#     visit new_user_session_path
+#     click_on "Sign up"
+#     fill_in "user[username]", with: "example-user"
+#     fill_in "user[email]", with: "example@user.com"
+#     fill_in "user[password]", with: "password"
+#     fill_in "user[password_confirmation]", with: "password"
+#     click_on "Create Account"
+
+#     expect(page).to have_content "Sign out"
+#     expect(page).not_to have_content "Sign up"
+#   end
+# end
