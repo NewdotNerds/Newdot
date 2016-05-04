833f96bbc36a2eb4bcfdc592a4b3dd2c531a5412
Omar Torres <torresomar44@gmail.com>
2016-05-04 11:03:12 -0500

    Change process flow. Make users don't whatch content if they not signed in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,4 +41,9 @@ module ApplicationHelper
     link_to text, url, options
   end
 
+  def image_link_to(image_path, url, image_tag_options = { }, link_to_options = { })
+    link_to url, link_to_options do
+      image_tag image_path, image_tag_options
+    end
+  end
 end
