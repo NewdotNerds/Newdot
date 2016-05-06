63f0aadd9e649e2339892a6f2242ad060918cef7
Omar Torres <torresomar44@gmail.com>
2016-05-05 23:43:17 -0500

    Minor update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,8 @@ if Rails.env.production?
   	config.fog_credentials = {
   		:provider			   => 'AWS',
   		:aws_access_key_id	   => ENV['S3_ACCESS_KEY'],
-  		:aws_secret_access_key => ENV['S3_SECRET_KEY']
+  		:aws_secret_access_key => ENV['S3_SECRET_KEY'],
+  		:region                 => 'sa-east-1'
   	}
   	config.fog_directory = ENV['S3_BUCKET']
   end
