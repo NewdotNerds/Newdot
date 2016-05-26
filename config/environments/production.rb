bbddc97c84e2a469147ab44a14afc774bd1a8359
Omar Torres <omartorres@MacBook-Air-de-MAC.local>
2016-05-25 23:41:27 -0500

    Add new logo and heroku host url for production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tranquil-gorge-66749.herokuapp.com'
+  host = 'tongue.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
