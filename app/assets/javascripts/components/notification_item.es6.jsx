940d127986de4d16f6679766f9c04aa8bbc1b9a5
Omar Torres <torresomar44@gmail.com>
2016-05-29 18:59:10 -0500

    Change heart icon to tong icon for notification container

--- app/assets/javascripts/components/notification_item.es6.jsx
+++ app/assets/javascripts/components/notification_item.es6.jsx
@@ -43,8 +43,12 @@ class NotificationItem extends React.Component {
   // FIXME: this is tightly coupled to action.
   notificationIcon() {
     switch(this.props.action) {
-      case 'le gustó tu':
-        return <i className="fa fa-heart-o"></i>;
+      case 'le dio tong a tu':
+        return <svg className="fa-tong" version="1.1" id="Capa_1" x="0px" y="0px" width="13.8551px" height="11.1818px" viewBox="-0.518 0 50.5 60.68" enable-background="new -0.518 0 50.5 60.68">
+            <path fill="#FF3A46" stroke="#FF3A46" strokeWidth="0.5" stroke-miterlimit="10" d="M4.307,2.671c3.1-3,14.5-0.5,14.5-0.5
+              c0.3,0,3,0.5,3,3.899v21c0.2,4.4,5.8,4.101,5.7,0V6.171c0-3.5,2.7-4,3.1-4.101c0,0,11.4-2.6,14.5,0.5c10.3,10.2,0.3,55.8-19.6,57.3
+              C4.406,61.47-6.094,12.87,4.307,2.671z"/>
+          </svg>;
       case 'empezó a seguirte':
         return <i className="fa fa-user"></i>;
       case 'respondió tu':
