97e0c50df677b41d5e9756cbc6ad7c7b559f0ac4
Omar Torres <torresomar44@gmail.com>
2016-06-27 14:11:17 -0500

    Set up gmail for sending emails in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'tongue.herokuapp.com'
+  host = 'tongs.co'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
+    :domain         => 'tongs.co',
     :enable_starttls_auto => true
   }
 
