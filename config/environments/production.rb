bdcbcbf0c5dc0e44ca5fbb90e66e0d682d24ca44
Omar Torres <torresomar44@gmail.com>
2016-06-27 15:45:18 -0500

    Change domain value to gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['GMAIL_USERNAME'],
     :password       => ENV['GMAIL_PASSWORD'],
-    :domain         => 'tongs.co',
+    :domain         => 'gmail.com',
     :enable_starttls_auto => true
   }
 
