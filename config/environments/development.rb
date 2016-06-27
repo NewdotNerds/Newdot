ebf9328b2e5784bc22a3306146a657aaac2f2a96
Omar Torres <torresomar44@gmail.com>
2016-06-27 12:04:59 -0500

    Set up SendGrid in production using Heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,16 @@ Rails.application.configure do
 
   # Action Mailer configuration
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: "587",
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME_DEV'],
+    password: ENV['GMAIL_PASSWORD_DEV']
+  }
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'tongue.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'www.tongs.co' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
