6bf2c84b5e09edd37d26f1e45e5f25c594651f1f
Omar Torres <torresomar44@gmail.com>
2016-06-28 13:11:52 -0500

    Add secrets.yml to .gitignore

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,13 @@ module Myapp
 
     config.active_job.queue_adapter = :sidekiq
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'secrets.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
