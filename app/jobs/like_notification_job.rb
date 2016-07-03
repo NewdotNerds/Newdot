0bd470ac28dc160b70b39730411b95ba2e173f1e
Omar Torres <torresomar44@gmail.com>
2016-07-03 17:17:27 -0500

    Reference liker actor in perform method of LikeNotification

--- app/jobs/like_notification_job.rb
+++ app/jobs/like_notification_job.rb
@@ -3,6 +3,8 @@ class LikeNotificationJob < ActiveJob::Base
 
   def perform(user_id)
     user = User.find_by(id: user_id)
+    liker = current_user
+    post = @post
     LikeMailer.like_notification(liker, post).deliver_now if user
   end
 end
