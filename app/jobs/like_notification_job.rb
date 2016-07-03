666a358306025fc142b30de03545aa0266614d34
Omar Torres <torresomar44@gmail.com>
2016-07-03 18:03:13 -0500

    Fix find method for post

--- app/jobs/like_notification_job.rb
+++ app/jobs/like_notification_job.rb
@@ -1,9 +1,9 @@
 class LikeNotificationJob < ActiveJob::Base
   queue_as :mailer
 
-  def perform(user_id)
+  def perform(user_id, post_id)
     user = User.find_by(id: user_id)
-    post = Post.find(params[:post_id])
+    post = Post.find_by(id: post_id)
     liker = @user
     LikeMailer.like_notification(liker, post).deliver_now if user
   end
