f353ce2293101df04e33906465b52bdbd81cf2ed
Omar Torres <torresomar44@gmail.com>
2016-07-08 16:33:55 -0500

    Add except: show to posts controller for testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :authorize_user, only: [:edit, :update, :destroy]
 
   impressionist actions: [:show], unique: [:session_hash]
