dadc77af4391073bb275bfbbd1d75661fd7b761e
Omar Torres <torresomar44@gmail.com>
2016-07-09 16:28:34 -0500

    Redirect to a specific page on successful sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
+  end
+
   protected
 
     def configure_permitted_parameters
