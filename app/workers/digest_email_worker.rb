24522f511da0c576cb04444f372fcc24d9c03a0c
Omar Torres <torresomar44@gmail.com>
2016-07-13 12:14:34 -0500

    Disable DigestMailer

--- app/workers/digest_email_worker.rb
+++ app/workers/digest_email_worker.rb
@@ -1,14 +1,14 @@
-class DigestEmailWorker
-  include Sidekiq::Worker
-  include Sidetiq::Schedulable
-  sidekiq_options :queue => :mailer
+#class DigestEmailWorker
+#  include Sidekiq::Worker
+#  include Sidetiq::Schedulable
+#  sidekiq_options :queue => :mailer
 
-  recurrence { weekly.day(:wednesday) }
+#  recurrence { weekly.day(:wednesday) }
 
 
-  def perform
-    User.find_each do |user|
-      DigestMailer.daily_email(user).deliver_now
-    end
-  end
-end
+#  def perform
+#    User.find_each do |user|
+#      DigestMailer.daily_email(user).deliver_now
+#    end
+#  end
+#end
