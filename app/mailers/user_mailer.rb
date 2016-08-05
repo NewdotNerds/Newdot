49ea02ee07c202db244b95cd2693f014695fa79c
Omar Torres <torresomar44@gmail.com>
2016-08-04 23:47:31 -0500

    Change welcome mail view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,10 @@
 class UserMailer < ApplicationMailer
+  add_template_helper(UsersHelper)
+  add_template_helper(PostsHelper)
+
   def welcome_email(user)
     @user = user
-    mail to: @user.email, subject: "Bienvenido a Tongs " + @user.username.split(" ").first
+    @topstories = Post.top_stories(3).published
+    mail to: @user.email, subject: "Tongs"
   end
 end
