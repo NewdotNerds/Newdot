6da609b8a6d3f15c828811b5a05aed8b99a3f32e
Omar Torres <torresomar44@gmail.com>
2016-08-20 21:38:26 -0500

    minor update

--- app/controllers/api/search_autocomplete_controller.rb
+++ app/controllers/api/search_autocomplete_controller.rb
@@ -1,8 +1,7 @@
 class API::SearchAutocompleteController < ApplicationController
   def index
-    results = Elasticsearch::Model.search(params[:term], [Post, User, Tag])
+    results = Elasticsearch::Model.search(params[:term], [Post, User])
     @posts = results.select { |result| result["_type"] == 'post' }
     @users = results.select { |result| result["_type"] == 'user' }
-    #@tags  = results.select { |result| result["_type"] == 'tag' }
   end
 end
