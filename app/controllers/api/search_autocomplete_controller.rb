da7aaad9fe425666f9774e7b469940580e88fd57
Omar Torres <torresomar44@gmail.com>
2016-08-20 21:33:22 -0500

    Comment out tag results in SearchAutocompleteController API

--- app/controllers/api/search_autocomplete_controller.rb
+++ app/controllers/api/search_autocomplete_controller.rb
@@ -3,6 +3,6 @@ class API::SearchAutocompleteController < ApplicationController
     results = Elasticsearch::Model.search(params[:term], [Post, User, Tag])
     @posts = results.select { |result| result["_type"] == 'post' }
     @users = results.select { |result| result["_type"] == 'user' }
-    @tags  = results.select { |result| result["_type"] == 'tag' }
+    #@tags  = results.select { |result| result["_type"] == 'tag' }
   end
 end
