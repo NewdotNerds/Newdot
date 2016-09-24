13db1a4f759b410603fd895513f20edf416fba7a
Omar Torres <torresomar44@gmail.com>
2016-09-24 17:18:07 -0500

    Allow users to edit their username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:description, :avatar, :location)
+      params.require(:user).permit(:username, :description, :avatar, :location)
     end
 
     def authorize_user
