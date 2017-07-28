8be7e158667a7c4cf0263351fbfadd0967148294
Omar Torres <torresomar44@gmail.com>
2017-07-28 13:25:08 -0500

    Some changes in UI

--- app/assets/javascripts/components/like_button.es6.jsx
+++ app/assets/javascripts/components/like_button.es6.jsx
@@ -40,7 +40,7 @@ class LikeButton extends React.Component {
       return (
         <button className="unlike-button" onClick={this.onUnlikeClick}>
           <svg className="animated bounceIn" version="1.1" id="Capa_1" x="0px" y="0px" width="33.8551px" height="39.1818px" viewBox="-0.518 0 50.5 60.68" enable-background="new -0.518 0 50.5 60.68">
-            <path fill="#EE4266" stroke="#EE4266" strokeWidth="0.5" stroke-miterlimit="10" d="M4.307,2.671c3.1-3,14.5-0.5,14.5-0.5
+            <path fill="#04bf75" stroke="#04bf75" strokeWidth="0.5" stroke-miterlimit="10" d="M4.307,2.671c3.1-3,14.5-0.5,14.5-0.5
               c0.3,0,3,0.5,3,3.899v21c0.2,4.4,5.8,4.101,5.7,0V6.171c0-3.5,2.7-4,3.1-4.101c0,0,11.4-2.6,14.5,0.5c10.3,10.2,0.3,55.8-19.6,57.3
               C4.406,61.47-6.094,12.87,4.307,2.671z"/>
           </svg>
@@ -50,7 +50,7 @@ class LikeButton extends React.Component {
       return (
         <button className="like-button" onClick={this.onLikeClick}>
           <svg version="1.1" id="Capa_1" x="0px" y="0px" width="33.8551px" height="39.1818px" viewBox="0 0 54.25 64.25" enable-background="new 0 0 54.25 64.25">
-            <path fill="#FFFFFF" stroke="#EE4266" strokeWidth="4" stroke-miterlimit="10" d="M6.75,4.351c3.1-3,14.5-0.5,14.5-0.5
+            <path fill="#FFFFFF" stroke="#04bf75" strokeWidth="4" stroke-miterlimit="10" d="M6.75,4.351c3.1-3,14.5-0.5,14.5-0.5
               c0.3,0,3,0.5,3,3.899v21c0.2,4.4,5.8,4.101,5.7,0V7.851c0-3.5,2.7-4,3.1-4.101c0,0,11.4-2.6,14.5,0.5c10.3,10.2,0.3,55.8-19.6,57.3
               C6.85,63.15-3.65,14.55,6.75,4.351z"/>
           </svg>
